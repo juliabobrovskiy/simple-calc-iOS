4465735b6f973bd75007af762d43010bc40641a4
Julia Bobrovskiy <jvb8@uw.edu>
2018-10-16 08:19:48 -0700

    add first code

---
+++ simple-calc/simple-calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simple-calc
+//
+//  Created by Julia Bobrovskiy on 10/16/18.
+//  Copyright © 2018 Julia Bobrovskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
